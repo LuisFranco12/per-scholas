cfe4726e8b97997ebaf2f25e35308fcac238837d
LuisFranco12 <luisf3525@gmail.com>
2023-01-26 17:11:53 -0500

    another commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 New commit
+
+another commit
--- mini-project/app.js
+++ mini-project/app.js
@@ -4,32 +4,6 @@ class Ship {
         this.firepower = firepower;
         this.accuracy = accuracy;
     }
-
-   playerAttack(alien) {
-        while(alien.hull > 0 || ussAssembly.hull > 0) {
-            if(Math.random() < alien.accuracy) {
-
-                console.log('Miss, player has been hit!')
-                ussAssembly.hull -= alien.firepower
-                console.log(ussAssembly.hull)
-                console.log('\n')
-                if(ussAssembly.hull <= 0) {
-                    return
-                }
-
-            }else {
-                console.log('Alien hit!')
-                alien.hull -= ussAssembly.firepower
-                console.log('\n')
-                if(alien.hull <= 0) {
-                    console.log('Alien health: 0' )
-                    console.log('you win this round')
-                    console.log('\n')
-                    return
-                }
-            }
-        }
-    }  
 }
 
 const ussAssembly = new Ship(20, 5, 0.7)
@@ -50,9 +24,7 @@ alienArray.push(alienShip5)
 const alienShip6 = new Ship (3 , 2, .6)
 alienArray.push(alienShip6)
 
-alienArray.forEach(alien => {
-    ussAssembly.playerAttack(alien);
-    if(ussAssembly.hull <= 0) return console.log('you lose')
 
-    if(alien.hull <= 0) return
-})
+playGame() {
+    alienArray.forEach()
+}
